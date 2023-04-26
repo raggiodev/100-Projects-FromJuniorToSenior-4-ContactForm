b9b3cfe353a54144c73ad5296fe9c351f4e72ba6
JoeTheorium <fernandoandresraggio@gmail.com>
2023-04-25 23:26:35 -0300

    First project with React

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/100-Projects-FromJuniorToSenior-4-ContactForm/"
+});
